50c6bd8422188e82f34cfb1d721039f172706837
MBHM9 <mohammadbhm8@gmail.com>
2023-04-13 15:15:24 +0330

    creat admin file and function find user and admin when login

--- src/Flight_schedule.java
+++ src/Flight_schedule.java
@@ -8,4 +8,82 @@ public class Flight_schedule {
     private int price;
     private int seats;
 
+    public Flight_schedule(String flight_id, String origin, String destination, String date, String time, int price, int seats) {
+        this.flight_id = flight_id;
+        this.origin = origin;
+        this.destination = destination;
+        this.date = date;
+        this.time = time;
+        this.price = price;
+        this.seats = seats;
+    }
+
+    public String getFlight_id() {
+        return flight_id;
+    }
+
+    public void setFlight_id(String flight_id) {
+        this.flight_id = flight_id;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    @Override
+    public String toString() {
+        return "Flight_schedule{" +
+                "flight_id='" + flight_id + '\'' +
+                ", origin='" + origin + '\'' +
+                ", destination='" + destination + '\'' +
+                ", date='" + date + '\'' +
+                ", time='" + time + '\'' +
+                ", price=" + price +
+                ", seats=" + seats +
+                '}';
+    }
 }
