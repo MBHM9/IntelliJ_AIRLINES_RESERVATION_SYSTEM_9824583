61a2d1779195473fe9ec3c76dfaf46b1f6956419
MBHM9 <mohammadbhm8@gmail.com>
2023-04-13 18:03:03 +0330

    setup "PASSENGER MENU OPTION" and login admin

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,24 @@ public class Main {
     public static void main(String[] args) {
 
         Flight_schedule[] Flight_schedule = new Flight_schedule[1000];
-        Flight_schedule[0] = new Flight_schedule("WX-12", "Yazd", "Tehran", "1402-01-27", "22:30", 700, 41);
-        User_Pass[] User_Pass = new User_Pass[10000];
-        User_Pass[0] = new User_Pass("mohammad", "1234", 0, null);
-        User_Pass_admin[] User_Pass_admin = new User_Pass_admin[10];
 
+
+        Flight_schedule[0] = new Flight_schedule("WX-12", "Yazd", "Tehran", "1402-01-27", "22:30", 700, 41);
+        Flight_schedule[1] = new Flight_schedule("GA-12", "Yazd", "Esfahan", "1402-05-27", "00:30", 300, 150);
+
+
+        User_Pass[] User_Pass = new User_Pass[10000];
+        User_Pass[0] = new User_Pass("mohammad", "1234", 5000, null);
+        User_Pass[0] = new User_Pass("Ali", "5863", 1500, null);
+
+        User_Pass_admin[] User_Pass_admin = new User_Pass_admin[10];
+        User_Pass_admin[0] = new User_Pass_admin("admin1", "123456");
+        User_Pass_admin[1] = new User_Pass_admin("admin2", "78910");
 
         User_Pass user_enter_data = new User_Pass(null, null, 0, null);
+        User_Pass_admin admin_enter_data = new User_Pass_admin(null, null);
+
+
         Login Login = new Login(User_Pass, User_Pass_admin);
 
 
@@ -31,9 +42,29 @@ public class Main {
                 String pass_enter = input.next();
                 user_enter_data = Login.find_user(user_enter, pass_enter);
                 if (user_enter_data == null) {
-                    System.out.println("user not found!");
+                    admin_enter_data = Login.find_admin(user_enter, pass_enter);
+                    if(admin_enter_data == null){
+                        System.out.println("user not found!");
+                    }else{
+                        /////////////start admin menu
+
+                    }
                 } else {
-                    System.out.println(user_enter_data.toString());
+                   /////////////start passenger menu
+                    boolean passenger_menu = true;
+                    while (passenger_menu) {
+                        System.out.println("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+                        System.out.println("                  PASSENGER MENU OPTION");
+                        System.out.println("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+                        System.out.println("..........................................................");
+                        System.out.println("<1> Change password");
+                        System.out.println("<2> Search flight tickets");
+                        System.out.println("<3> Booking ticket");
+                        System.out.println("<4> Ticket cancellation");
+                        System.out.println("<5> Booked ticket");
+                        System.out.println("<6> Add charge");
+                        System.out.println("<0> Sign out");
+                    }
                 }
             }
         }
